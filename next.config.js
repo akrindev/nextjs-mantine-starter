93941805c1b4bb3535e5cab0e25454efddeb85cb
Syakirin Amin <akrinmin@gmail.com>
2023-11-08 10:31:34 +0700

    fix: clean up
    Signed-off-by: Syakirin Amin <akrinmin@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
+const bundleAnalyzer = require("@next/bundle-analyzer");
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
+    webpackBuildWorker: true,
   },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
